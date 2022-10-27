6f5339f8d803a2806ff5c46a10293e2c75dd0367
Emorinken <emorinkensam@gmail.com>
2022-10-27 00:43:10 -0700

    added the slack name hidden to the profile section

--- src/components/LinkSection.jsx
+++ src/components/LinkSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const LinkSection = () => {
   return (
 	<section className="">
--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -5,14 +5,8 @@ const ProfileSection = () => {
   return (
 	<header className="h-[192px] w-full flex justify-center flex-col items-center relative">
 		<img src={profile} id="profile__img" alt="profile-picture" className='h-24 w-24 rounded-full' />
-		<h4 className='twitter-text mt-4 font-bold text-[20px]'>Emor Musk</h4>
-		<svg className='flex md:hidden absolute right-0 top-4' width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="1" y="1" width="40" height="40" rx="20" fill="white"/>
-<path d="M21 21.8333C21.4602 21.8333 21.8333 21.4602 21.8333 21C21.8333 20.5398 21.4602 20.1667 21 20.1667C20.5397 20.1667 20.1666 20.5398 20.1666 21C20.1666 21.4602 20.5397 21.8333 21 21.8333Z" stroke="#98A2B3" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M26.8333 21.8333C27.2935 21.8333 27.6666 21.4602 27.6666 21C27.6666 20.5398 27.2935 20.1667 26.8333 20.1667C26.3731 20.1667 26 20.5398 26 21C26 21.4602 26.3731 21.8333 26.8333 21.8333Z" stroke="#98A2B3" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M15.1666 21.8333C15.6269 21.8333 16 21.4602 16 21C16 20.5398 15.6269 20.1667 15.1666 20.1667C14.7064 20.1667 14.3333 20.5398 14.3333 21C14.3333 21.4602 14.7064 21.8333 15.1666 21.8333Z" stroke="#98A2B3" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-<rect x="1" y="1" width="40" height="40" rx="20" stroke="#D0D5DD" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="1 3"/>
-		</svg>
+		<h4 className='twitter-text mt-4 font-bold text-[20px]' id='twitter'>Tech Broski</h4>
+		<input type="hidden" name="samuel emorinken" id='slack' />
 
 	</header>
   )
