d75ccc94c9d503675cbda791752f128462d52662
Emorinken <emorinkensam@gmail.com>
2022-10-27 00:47:04 -0700

    added comments describing code

--- src/components/LinkSection.jsx
+++ src/components/LinkSection.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 
 const LinkSection = () => {
+	//this section should contain anchor tags wit links set in the href redirecting to other sites 
+	// i used an unordereed list to display the list of anchor tags and styled thhem as buttons 
+
   return (
 	<section className="">
 		<ul className="w-full list-none ">
-			<li id='twitter' className="h-[76px] flex justify-center rounded-md items-center links bg-gray-200 text-gray-900 font-semibold"><a href="https://twitter.com/Emor__Musk">Twitter Link</a></li>
+			<li className="h-[76px] flex justify-center rounded-md items-center links bg-gray-200 text-gray-900 font-semibold"><a href="https://twitter.com/Emor__Musk">Twitter Link</a></li>
 			<li id='btn__zuri' className="h-[76px] flex my-4 rounded-md justify-center items-center links bg-gray-200 text-gray-900 font-semibold"><a href="https://training.zuri.team/">Zuri Team</a></li>
 			<li id='books' className="h-[76px] flex my-4 rounded-md justify-center items-center links bg-gray-200 text-gray-900 font-semibold"><a href=" http://books.zuri.team">Zuri Books</a></li>
 			<li id='book__python' className="h-[76px] flex my-4 rounded-md justify-center items-center links bg-gray-200 text-gray-900 font-semibold"><a href="https://books.zuri.team">Python Books</a></li>
--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import profile from '../images/Photograph.jpg'
 
 const ProfileSection = () => {
+	//profile picture with id profile__img
+	//Your twitter username with an id ‘twitter’
+	//Your slack name with an id ‘slack’ - but this should be hidden (available in code, but cannot be seen)
   return (
 	<header className="h-[192px] w-full flex justify-center flex-col items-center relative">
 		<img src={profile} id="profile__img" alt="profile-picture" className='h-24 w-24 rounded-full' />
