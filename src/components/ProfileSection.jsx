787d2f7a4617831fedafe125d5d9a2d1c3cfdc05
Emorinken <emorinkensam@gmail.com>
2022-10-28 16:09:35 -0700

    changed slack username

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -9,7 +9,7 @@ const ProfileSection = () => {
 	<header className="h-[192px] w-full flex justify-center flex-col items-center relative">
 		<img src={profile} id="profile__img" alt="profile-picture" className='h-24 w-24 rounded-full' />
 		<h4 className='twitter-text mt-4 font-bold text-[20px]' id='twitter'>Tech Broski</h4>
-		<input type="hidden" name="samuel emorinken" id='slack' />
+		<input type="hidden" name="Emor_Musk" id='slack' />
 
 	</header>
   )
